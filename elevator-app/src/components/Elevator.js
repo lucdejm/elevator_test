b719de1d07a974ffded10ba0b91d58256c925aa3
lucdejems <luc.dejems@gmail.com>
2018-09-14 23:01:49 +0200

    setup elevator app

---
+++ elevator-app/src/components/Elevator.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Elevator extends Component {
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
+}
+
+export default Elevator;
